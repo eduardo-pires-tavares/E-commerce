5be6e420165f8ceb7663acc5441c2719cf10ad5e
Eduardo Tavares <eduardo.pires.tavares@gmail.com>
2021-01-27 02:51:06 +0000

    Added nested routing for full collection pages, and some styling to them

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ class App extends Component<AppProps, {}> {
 				<Header />
 				<Switch>
 					<Route exact path='/' component={Homepage} />
-					<Route exact path='/shop' component={ShopPage} />
+					<Route path='/shop' component={ShopPage} />
 					<Route exact path='/checkout' component={CheckoutPage} />
 					<Route
 						path='/signin'
--- src/pages/collection/index.tsx
+++ src/pages/collection/index.tsx
@@ -1,12 +1,39 @@
 import "./index.styles.scss";
 import { RouteComponentProps } from "react-router-dom";
+import { ITEMS } from "../../store/shop/types";
+import { createStructuredSelector } from "reselect";
+import { ApplicationState } from "../../store";
+import { IShopSelector, selectShopData } from "../../store/shop/selectors";
+import { connect, ConnectedProps } from "react-redux";
+import CollectionItem from "../../components/collection-item";
 
-const Collection = ({ match }: RouteComponentProps) => {
+interface RouteInfo {
+	collection: string;
+}
+
+const Collection = ({ collections, match }: CollectionProps) => {
+	const collection = collections?.find(col => col.routeName === match.params.collection);
+
 	return (
-		<>
-			<h1>{match.path}</h1>
-		</>
+		<div>
+			<div className='collection'>
+				<h1 className='title'>{collection?.title.toUpperCase()}</h1>
+				<div className='collection-full'>
+					{collection?.items?.map(({ id, ...otherComponentProps }: ITEMS) => {
+						return <CollectionItem key={id} id={id} {...otherComponentProps} />;
+					})}
+				</div>
+			</div>
+		</div>
 	);
 };
 
-export default Collection;
+const mapStateToProps = createStructuredSelector<ApplicationState, IShopSelector>({
+	collections: selectShopData,
+});
+
+const connector = connect(mapStateToProps);
+
+type CollectionProps = ConnectedProps<typeof connector> & RouteComponentProps<RouteInfo>;
+
+export default connector(Collection);
